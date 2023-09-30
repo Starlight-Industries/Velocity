65bad1548412ffe977b195e518f97e8d0d54f437
Cazdotsys <caznix01@gmail.com>
2023-09-29 23:04:15 -0400

    Added support for seperate slint files

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
+slint::include_modules!();
 fn main() {
+    onboard::new().unwrap().run().unwrap();
     MainWindow::new().unwrap().run().unwrap();
 }
 
 slint::slint! {
     export component MainWindow inherits Window {
+        width: 1280px;
+        height: 720px;
+        background: #161219;
         title: "Velocity 0.1";
         icon: @image-url("src/assets/icons/Velocity_256x.png");
     }
