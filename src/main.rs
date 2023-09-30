bf35b6a0580ca78b242e5461021cd1f29378d462
Cazdotsys <caznix01@gmail.com>
2023-09-29 21:06:33 -0400

    Test commit

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    MainWindow::new().unwrap().run().unwrap();
+}
+
+slint::slint! {
+    export component MainWindow inherits Window {
+        title: "Velocity 0.1";
+        icon: @image-url("src/assets/icons/Velocity_256x.png");
+    }
+}d
